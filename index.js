c79eac6de2d53f9bc12d40ba299de3c112df8fc2
Luis Perez <luis0@stanford.edu>
2018-08-01 11:13:11 -0700

    Set up API passthrough

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const Zillow = require("node-zillow");
+const availableEndpoints = require('./lib/api-list');
 
 const app = express();
+
+const zillow = new Zillow("X1-ZWz1gklptnjw97_5xiff", {
+	https: true
+});
 
 
 // Serve static content in public/ directory.
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Set-up templates.
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
   
-app.get('/', (req, res) => res.render('pages/index'))
+// Root page will render the pages/index, which will be our main lenslet.
+app.get('/', (req, res) => res.render('pages/index'));
+
+// Setup an api route in express.
+// /api/GetResults will set req.params = 'GetResults'
+app.get('/api/:endpoint', (req, res) => {
+	const endpoint = req.params.endpoint;
+	if (!(endpoint in availableEndpoints)) {
+		return res.json({
+			'error': 'Invalid endpoint requested: ' + endpoint,
+			'availableEndpoints': availableEndpoints,
+		});
+	}
+	console.log(req.query);
+	// Otherwise, forward the requqest and parameters to zillow library.
+	response = zillow.get(req.params.endpoint, req.query);
+	res.json(response);
+});
+
+// Set-up some of the API.
+
+// We want to set-up an API route that will forward our requests to Zillow in
+// order to avoid API
 
 // Listen on the given port.
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
